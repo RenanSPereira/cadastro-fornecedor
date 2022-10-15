c9b1cffc362417b9a3718eace8d16a0115e4724b
RenanSPereira <renanspereira.dev@gmail.com>
2022-10-15 20:03:11 -0300

    Criando teste para remoção de Fornecedor

--- Test/CadastroFornecedor.Test/FornecedorRepositoryTest.cs
+++ Test/CadastroFornecedor.Test/FornecedorRepositoryTest.cs
@@ -18,7 +18,7 @@ public class FornecedorRepositoryTest
     [Fact]
     public async Task Deve_Retornar_Nulo_Caso_Fornecedor_Nao_Seja_Econtrado()
     {
-        _repositorio.ObterPorId((Arg.Any<Guid>())).ReturnsNull<Fornecedor>();
+        _repositorio.ObterPorId(Arg.Any<Guid>()).ReturnsNull<Fornecedor>();
 
         var fornecedor = await _repositorio.ObterPorId(Guid.NewGuid());
 
@@ -27,7 +27,7 @@ public class FornecedorRepositoryTest
 
     [Fact]
     public async Task Deve_Retornar_Um_Fornecedor_Caso_Ele_Exista()
-    {   
+    {
         //contexto
         var fornecedor = new Fornecedor("Nome Fantasia Teste", "Razao Social Teste",
             new Cnpj("38.071.355/0001-44"), new Endereco("Logradouro Teste", "Bairro Teste", "520C"),
@@ -40,5 +40,25 @@ public class FornecedorRepositoryTest
 
         //Verificação
         Assert.Equal(resultado.Id, fornecedor.Id);
+    }
+
+    [Fact]
+    public async Task Deve_Remover_O_Fornecedor()
+    {
+        //contexto
+        var fornecedor = new Fornecedor("Nome Fantasia Teste", "Razao Social Teste",
+            new Cnpj("38.071.355/0001-44"), new Endereco("Logradouro Teste", "Bairro Teste", "520C"),
+            TipoFornecedor.NaoPereciveis);
+
+        _repositorio
+            .When(x => x.Remover(fornecedor))
+            .Do(x => _repositorio.ObterPorId(fornecedor.Id).ReturnsNull());
+
+        //Ação
+        await _repositorio.Remover(fornecedor);
+        var resultado = await _repositorio.ObterPorId(fornecedor.Id);
+
+        //Verificação
+        Assert.Null(resultado);
     }
 }
