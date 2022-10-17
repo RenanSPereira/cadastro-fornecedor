f45050ed074416ca06e707bd344660de82c50b09
RenanSPereira <renanspereira.dev@gmail.com>
2022-10-17 17:19:48 -0300

    Possibilitando retorno nulo

--- Src/CadastroFornecedor.Api/Domain/Interfaces/IRepository.cs
+++ Src/CadastroFornecedor.Api/Domain/Interfaces/IRepository.cs
@@ -2,7 +2,7 @@ namespace CadastroFornecedor.Api.Domain.Interfaces;
 
 public interface IRepository<EntityBase> : IDisposable
 {
-    Task<EntityBase> ObterPorId(Guid id);
+    Task<EntityBase?> ObterPorId(Guid id);
     Task Adicionar(EntityBase entity);
     Task Atualizar(EntityBase entity);
     Task Remover(EntityBase entity);
